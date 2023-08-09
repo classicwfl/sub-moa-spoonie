2279e1d77322596abad241946dddaccc67e59f2b
Will Leffert <wleffert@audentio.com>
2023-08-08 20:43:10 -0500

    Initial commit much later than it should be because I'm a daredevil

---
+++ components/hero.tsx
@@ -0,0 +1,80 @@
+type Props = {
+    link: string
+    linkText: string
+}
+
+function NavLink({link, linkText}: Props ) {
+    return (
+        <a href={link} className="w-full h-full flex items-center rounded-full p-4 shadow-inactive hover:shadow-active hover:text-darkbg transition-all duration-300">
+            <span className="font-bighead uppercase text-center italic text-[5.8vw] 
+            sm:text-[5vw] md:text-[3.5vw] lg:text-[2.5vw] heromax:text-6xl leading-neg">{linkText}</span>
+        </a>
+    )
+}
+
+export default function Hero() {
+    return (
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 grid-rows-1 max-w-[1800px] m-auto">
+            <div className="aspect-square w-full border-white border-2 rounded-full p-1 lg:p-2">
+                <img src="/SubMOASpoonieLogoNoBorder.png" 
+                    alt="Sub-MOA Spoonie Logo" 
+                    className="aspect-square w-full rounded-full"
+                />
+            </div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden sm:block"></div>
+            <div className="aspect-square w-full border-white border-b-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hover:animate-spin">
+                <img src="/spoon1.png" 
+                    alt="" 
+                    className="aspect-square w-full rounded-full"
+                />
+            </div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden md:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden md:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full">
+                <NavLink link="/" linkText="Sub-MOA Spoonie Challenge" />
+            </div>
+            <div className="aspect-square w-full border-white border-l-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full block sm:hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden sm:block hover:animate-spin">
+                <img src="/cane2.png" 
+                    alt="" 
+                    className="aspect-square w-full rounded-full"
+                />
+            </div>
+            <div className="aspect-square w-full border-white border-t-2 rounded-full hidden md:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hover:animate-spin">
+                <img src="/spoon2.png" 
+                    alt="" 
+                    className="aspect-square w-full rounded-full"
+                />
+            </div>
+            <div className="aspect-square w-full border-white border-2 rounded-full">
+                <NavLink link="/" linkText="What is a Sub-MOA Spoonie" />
+            </div>
+            <div className="aspect-square w-full border-white border-r-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden md:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden sm:block"></div>
+            <div className="aspect-square w-full border-white border-r-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full">
+                <NavLink link="/" linkText="Sub-MOA Spoonie Merch" />
+            </div>
+            <div className="aspect-square w-full border-white border-t-2 rounded-full hidden lg:block"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-2 rounded-full hover:animate-spin">
+                <img src="/cane1.png" 
+                    alt="" 
+                    className="aspect-square w-full rounded-full"
+                />
+            </div>
+            <div className="aspect-square w-full border-white border-2 rounded-full"></div>
+            <div className="aspect-square w-full border-white border-t-2 rounded-full block sm:hidden"></div>
+        </div>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,63 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "accent-1": "#FAFAFA",
+                "accent-2": "#EAEAEA",
+                "accent-7": "#333",
+                "text-color": "#fff",
+                darkbg: "rgb(24, 26, 28)",
+            },
+            spacing: {
+                "13": "3.25rem",
+                "15": "3.75rem",
+                "30": "7.5rem",
+                "60": "15rem",
+                "128": "32rem",
+                "144": "36rem",
+            },
+            letterSpacing: {
+                tighter: "-.04em",
+            },
+            lineHeight: {
+                tight: "1",
+                neg: ".8",
+            },
+            fontFamily: {
+                sans: ["lorimer-no-2", "sans-serif"],
+                bighead: ["lorimer-no-2-condensed", "sans-serif"],
+            },
+            fontSize: {
+                "5xl": "2.5rem",
+                "6xl": "3.25rem",
+                "7xl": "4.5rem",
+                "8xl": "7.25rem",
+                "9xl": "9.25rem",
+            },
+            maxWidth: {
+                big: "1920px",
+            },
+            backgroundImage: {
+                inactive:
+                    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) .01%, rgba(255,255,255,.0) .02%, rgba(255,255,255,0) 100%);",
+                active: "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 99.98%, rgba(255,255,255,.0) 99.99%, rgba(255,255,255,0) 100%);",
+            },
+            boxShadow: {
+                inactive: "-0 0 0 0px rgb(255,255,255) inset",
+                active: "-0 0 0 600px rgb(255,255,255) inset",
+            },
+            screens: {
+                'heromax': '1800px',
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
